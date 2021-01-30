0450985fe8ede29269c272c6a9362612e7599661
north4web <69797932+north4web@users.noreply.github.com>
2021-01-30 13:37:01 +0100

    fixes

--- src/component/About/About.js
+++ src/component/About/About.js
@@ -7,11 +7,11 @@ function About() {
         <div>
 
             <div style={{ margin: "15px" }}>
-                <p>Hatify is an online shop for caps and hats. With over 5000 different models in stock, we have something to offer anyone looking for caps and hats. In addition, we have very fast delivery and caring customer service. We have chosen to specialize in headgear so that we can do it really well. By putting all our focus on headgear, we are able to offer a catalogue and a store that truly offers everything, and that makes it very simple and convenient to shop for a new hat or cap.</p>
+                <p>Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do eiusmod tempor incididunt ut labore et dolore magna aliqua. Ut enim ad minim veniam, quis nostrud exercitation ullamco laboris nisi ut aliquip ex ea commodo consequat. Duis aute irure dolor in reprehenderit in voluptate velit esse cillum dolore eu fugiat nulla pariatur. Excepteur sint occaecat cupidatat non proident, sunt in culpa qui officia deserunt mollit anim id est laborum.</p>
 
-                <p>Hatify was founded in 2015, and has since developed at a rapid pace. From running the whole operation out of a room of 10 square metres, we have grown so that our premises today are over 1000 square metres in area. Today Hatify has web-shops in Sweden, Denmark, Finland, Norway and Germany. Several tens of thousands of customers have already chosen Hatstore, with hundreds of new customers every day. We want to take care of both existing and new customers, so if you have any concerns to report or complaints to make, you are very welcome to contact us.</p>
+                <p>Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do eiusmod tempor incididunt ut labore et dolore magna aliqua. Ut enim ad minim veniam, quis nostrud exercitation ullamco laboris nisi ut aliquip ex ea commodo consequat. Duis aute irure dolor in reprehenderit in voluptate velit esse cillum dolore eu fugiat nulla pariatur. Excepteur sint occaecat cupidatat non proident, sunt in culpa qui officia deserunt mollit anim id est laborum.</p>
 
-                <p>Hatify is a company under constant development in all areas. We are continuously working to expand our product line and enhance our services. We do this so that we are always moving in the right direction, offering a store that is getting better with every passing day!</p>
+                <p>Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do eiusmod tempor incididunt ut labore et dolore magna aliqua. </p>
             </div>
 
 
--- src/component/Contact/Contact.js
+++ src/component/Contact/Contact.js
@@ -1,14 +1,16 @@
-import React, { useState } from 'react'
+import React, {useState} from 'react'
+
+
 
 function Contact() {
 
     const [state, setState] = useState({});
-
+    
 
     const updateState = (e) => {
         e.preventDefault();
         state[e.target.id] = e.target.value;
-        setState({ ...state });
+        setState({...state});
     }
 
     const getData = (e) => {
@@ -17,26 +19,38 @@ function Contact() {
         localStorage.setItem("John_Data", JSON.stringify(state));
         const myData = (localStorage.getItem("John_Data"));
         console.log(myData);
-    }
+        }
 
-    return (
-        <div>
+        return(
+            <div>
+               
+                
+<form style={{margin:"15px", textAlign: "center"}}>
 
-            <div style={{ margin: "15px" }}>
-                <h3>To contact us, please Fill in the form</h3>
-                <p></p>
 
-                <p>Name: <input type="text" className="form" id="brand" onChange={updateState} /></p>
-                <p>Email: <input type="email" className="form" id="color" onChange={updateState} />  </p>
-                <p>Message: <input type="text" className="form" id="year" onChange={updateState} /> </p>
+<h4>Send us a message</h4>
 
-    
-                <button onClick={getData}>Send</button>
 
-                <h4 style={{ color: 'navy' }}>{state.brand} {state.color} {state.year}</h4>
+<p>Name: < br/>
+    <input type="text" className="form"  onChange = {updateState}/></p>
+<p>Email: < br/>
+ <input size="100" type="email" className="form"  onChange = {updateState}/>  </p>
+Message:< br/>
+
+<textarea rows="3" cols="50"  onChange = {updateState}>
+</textarea>
+
+
+< br />
+<button onClick={getData}>Send</button>
+
+
+{/* <h4 style={{color: 'navy'}}>{state.brand} {state.color} {state.year}</h4> */}
+</form>
+
+
             </div>
-        </div>
-    )
-}
+        )
+    }
 
 export default Contact
