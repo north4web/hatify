1a029dbcc4588d504894955e3138b8ae1a6f60db
north4web <69797932+north4web@users.noreply.github.com>
2021-02-28 16:22:58 +0100

    fix

--- src/components/Pages/Hat-types-bucket/bucketsList.js
+++ src/components/Pages/Hat-types-bucket/bucketsList.js
@@ -111,8 +111,7 @@ export default function RecipeReviewCard() {
             <CardContent>
 
               <Typography paragraph>
-                Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do eiusmod tempor incididunt ut labore et dolore magna aliqua. Ut enim ad minim veniam, quis nostrud exercitation ullamco laboris nisi ut aliquip ex ea commodo consequat. Duis aute irure dolor in reprehenderit in voluptate velit esse cillum dolore eu fugiat nulla pariatur. Excepteur sint occaecat cupidatat non proident, sunt in culpa qui officia deserunt mollit anim id est laborum.
-
+                <p style={{width:"290px", textAlign:"justify"}}>Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do eiusmod tempor incididunt ut labore et dolore magna aliqua. </p>
               </Typography>
             </CardContent>
           </Collapse>
